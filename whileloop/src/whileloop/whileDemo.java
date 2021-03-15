d8f4ef7abb5736afc3bbf531385636c1172a4da1
vani455 <srivanipatchava@gmail.com>
2021-03-15 19:29:19 +0530

    vani

---
+++ whileloop/src/whileloop/whileDemo.java
@@ -0,0 +1,17 @@
+package whileloop;
+
+public class whileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		while(x<20)
+		{
+			System.out.print("value of x : " +x);
+			x++;
+			System.out.println("\n");
+		}
+
+	}
+
+}
